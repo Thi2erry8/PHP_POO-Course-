0c3cdf31ef5de82811a942bf4dc929a3862bd813
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-21 21:30:54 -0400

    lecon_4_end

--- form.php
+++ form.php
@@ -1,17 +1,33 @@
 <?php
-     class Form{
-           
-           private $data;
-           public $surround = 'p';
 
+    /*
+    *class Form
+    *Permet de generer un formulaire rapidement et simplement
+    */
+     class Form{
+           /*
+             *@Var array donnees utilisees par le formulaire
+           */
+           private $data;
+           /*
+             *@Var string utilise pour  contenir les champs
+           */
+           public $surround = 'p';
+           /*
+             *@param utilise pour  recuperer les donnees utiliser dans le formulaire
+           */
            public function __construct($data){
                 $this->data = $data;
            }
-           
+           /*
+             *@param code html a entourer
+           */
            private function surround($hmtl){
                return "<{$this->surround}>{$hmtl}</{$this->surround}>" ; 
            }
-
+           /*
+             *@param code html a entourer
+           */
            private function getValue($index){
                return isset($this->data[$index]) ? $this->data[$index] : null;
            }
