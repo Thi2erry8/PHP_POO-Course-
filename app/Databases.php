bc68890e58ddeffd07c81e5a9a4d78c21a2748d5
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-27 21:30:28 -0400

    010101

---
+++ app/Databases.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App;
+
+class Database{
+              
+         private $db_name;
+         private $db_user;
+         private $db_pass;
+         private $db_host;
+
+    public function __construct($db_name,$db_user='root',$db_host='localhost'){
+        
+    }
+
+}
--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,9 @@
 <?php
      $pdo = new PDO('mysql:dbname=blog;host=localhost','root','');
-     $pdo->exec('INSERT INTO articles SET titre="Mon titre", date="'. date('Y-m-d H:i:s') .'"');
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $res = $pdo->query('SELECT * FROM articles');
+     var_dump($res->fetchAll())
+     /* $count = $pdo->exec('INSERT INTO articles SET titre="Mon titre", date_d_ajout ="'. date('Y-m-d H:i:s') .'"');
+     var_dump($count);  */
 
-?>   
+?>      
