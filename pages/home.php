5c33b750087cc9d42dfdb676bfc7fd12b8b0865f
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-10-01 19:38:08 -0400

    001011

--- pages/home.php
+++ pages/home.php
@@ -2,8 +2,9 @@
      $pdo = new PDO('mysql:dbname=blog;host=localhost','root','');
      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      $res = $pdo->query('SELECT * FROM articles');
-     var_dump($res->fetchAll())
+     $datas = $res->fetchAll(PDO::FETCH_OBJ);
+     var_dump($datas[0]->titre);
      /* $count = $pdo->exec('INSERT INTO articles SET titre="Mon titre", date_d_ajout ="'. date('Y-m-d H:i:s') .'"');
      var_dump($count);  */
 
-?>      
+?>       
