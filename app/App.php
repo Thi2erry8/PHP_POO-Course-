4527f98c866e8c0e72a25f397bbe7a872d751454
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-10-07 19:57:49 -0400

    001010

--- app/App.php
+++ app/App.php
@@ -1,5 +1,19 @@
 <?php
      class App{
-         
-     }
+           
+        const DB_NAME = 'blog';
+        const DB_USER = 'root';
+        const DB_PASS = '';
+        const DB_HOST = 'localhost';
+
+        private static $database;
+
+        public static function getDatabase(){
+            if( self::$database === null ){
+                self::$database = new App\Database(self::DB_NAME, self::DB_USER,self::DB_PASS,self::DB_HOST);
+            }
+
+            return self::$database;
+        }
+     }  
 ?>
